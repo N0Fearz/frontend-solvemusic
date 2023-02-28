80afc795a2b284cfcfd59252516fa06c9d49b0a7
ps161753 <ps161753@summacollege.nl>
2023-02-28 14:20:36 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar';
-import DenseTable from './components/locationTable';
-import { Container } from '@mui/system';
 import Locations from './pages/Locations';
 import Settings from './pages/Settings';
 import React from 'react';
 import {
-  BrowserRouter,
   Routes,
   Route,
-  Switch
 } from "react-router-dom";
 
 function App() {
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,14 +1,84 @@
-import { Container } from '@mui/material';
+import { Container, FormControl, MenuItem, InputLabel, Button  } from '@mui/material';
 import React, {useState, useEffect} from 'react';
-import DenseTable from '../components/locationTable';
+import Select from '@mui/material/Select';
+import useAxios from 'axios-hooks'
+import { Box } from '@mui/system';
+import axios from 'axios';
 
 
 function Locations() {
+  const [chosenGenre, setChosenGenre] = React.useState('');
+  const [chosenCategory, setChosenCategory] = React.useState('');
+  const [genres, setGenres] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    return (
+
+  const [{ data: locationData, loading: locationLoad }] = useAxios(
+    "http://localhost:8080/locations/"
+  );
+
+  const [{ data: categoryData, loading: categoryLoad }] = useAxios(
+    "http://localhost:8080/category/"
+  );
+  useEffect(() => {
+    if (!locationLoad) {
+      setGenres(locationData);
+      console.log(locationData)
+    } 
+       }, [locationData]);
+
+    useEffect(() => {
+    if (!categoryLoad) {
+      setCategories(categoryData);
+      console.log(categoryData)
+      } 
+    }, [categoryData]);
+
+  const moveMusic = () =>{
+    axios.get('http://localhost:8080/move/get/files', {
+      locationId: chosenGenre,
+      categoryId: chosenCategory
+    })
+  }
+
+  return (
       
     <Container  sx={{mt:4}}>
-      <DenseTable/>
+    <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Location</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={chosenGenre}
+            label="Music location"
+            onChange={e=>setChosenGenre(e.target.value)}
+          >
+            {genres.map((value, index)=>{
+              return(
+                <MenuItem key={index} value={value.locationId}>{value.locationName}</MenuItem>)
+            })}
+          </Select>
+    </FormControl>
+
+    <FormControl fullWidth sx={{mt:4}}>
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={chosenCategory}
+            label="Category"
+            onChange={e=>setChosenCategory(e.target.value)}
+          >
+            
+            {categories.map((value, index)=>{
+              return(
+                <MenuItem key={index} value={value.categoryId}>{value.categoryName}</MenuItem>)
+            })}
+          </Select>
+    </FormControl>
+    <Box display="flex" justifyContent="center" alignItems="center">
+      <Button variant="contained" sx={{mt:4}} >Move music</Button>
+    </Box>
     </Container>
     );
 }
